5c8a0b170d532212deb2b34691c165b038eb6182
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-03-08 17:05:54 +0900

    readme revised

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 **Based on the code written by [Hyung-Kwon Ko](https://github.com/hyungkwonko/umato)**
 
+### Installation
+
+```
+pip3 install metrics4mdp
+```
+
+### Usage
+
+
+#### Example
+```python
+from metric4mdp import MDPMetricProvider
+
+mlist = [""DTM", "DTM_KL1", "Continuity", "Trustworthiness"]
+metricprovider = MDPMetricProvider(raw_data, emb_data, mlist, k=7)
+
+
+```
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
  
 setup(
     name                = 'metrics4mdp',
-    version             = '0.1',
+    version             = '0.1.1',
     description         = 'Quantitative metrics of Multidimensional Projections (MDP) in Python',
     author              = 'Hyeon Jeon',
     author_email        = 'hj@hcil.snu.ac.kr',
     url                 = 'https://github.com/hj-n/metrics4mdp',
     download_url        = 'https://github.com/hj-n/metrics4mdp',
-    install_requires    =  [numpy, scipym sklearn, numba],
+    install_requires    =  ['numpy', 'scipy', 'sklearn', 'numba'],
     packages            = find_packages(exclude = []),
     keywords            = ['MDP', 'Multidimensional Projections', "Dimensionality Reduction", "metric"],
     python_requires     = '>=3',
