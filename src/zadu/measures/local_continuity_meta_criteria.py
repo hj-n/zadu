8bb9599aedc472fa6d2e1b52ef5edf41cef50f64
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-06 19:01:33 +0900

    added local continuity meta critera

---
+++ src/zadu/measures/local_continuity_meta_criteria.py
@@ -0,0 +1,32 @@
+import numpy as np
+from .utils import knn
+
+
+def local_continuity_meta_criteria(orig, emb, k, knn_info=None):
+  """
+  Compute the local continuity meta-criteria of the embedding
+  INPUT:
+		ndarray: orig: original data
+		ndarray: emb: embedded data
+		int: k: number of nearest neighbors to consider
+		tuple: knn_info: precomputed k-nearest neighbors and rankings of the original and embedded data (Optional)
+  OUTPUT:
+		dict: local continuity meta-criteria
+  """
+  if knn_info is None:
+    orig_knn_indices = knn.knn(orig, k)
+    emb_knn_indices = knn.knn(emb, k)
+  else:
+    orig_knn_indices, emb_knn_indices = knn_info
+
+  point_num = orig.shape[0]
+  
+  value = 0.0
+  for i in range(point_num):
+    value += np.intersect1d(orig_knn_indices[i], emb_knn_indices[i]).shape[0] - ((k * k) / (point_num - 1))
+  
+  lcmc = value / (point_num * k)
+  return {
+    "lcmc": lcmc
+	}
+     
