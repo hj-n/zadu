841a4f1cd34a20c3fdde9a9d5a0af3135dffcd90
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-06 19:38:32 +0900

    added class-aware continuity and trustworthiness

---
+++ src/zadu/measures/class_aware_trustworthiness_continuity.py
@@ -0,0 +1,53 @@
+import numpy as np
+from .utils import knn
+
+def class_aware_trustworthiness_continuity(orig, emb, label, k, knn_ranking_info=None):
+	"""
+	Compute class-aware trustworthiness and continuity of the embedding
+	INPUT:
+		ndarray: orig: original data
+		ndarray: emb: embedded data
+		int: k: number of nearest neighbors to consider
+		tuple: knn_ranking_info: precomputed k-nearest neighbors and rankings of the original and embedded data (Optional)
+	OUTPUT:
+		dict: class-aware trustworthiness (ca_trustworthiness) and class-aware continuity (ca_continuity)
+	"""
+
+	if knn_ranking_info is None:
+		orig_knn_indices, orig_ranking = knn.knn_with_ranking(orig, k)
+		emb_knn_indices,  emb_ranking  = knn.knn_with_ranking(emb, k)
+	else:
+		orig_knn_indices, orig_ranking, emb_knn_indices, emb_ranking = knn_ranking_info
+	
+	## class-aware trustworthiness
+	ca_trust = ca_tnc_computation(orig_knn_indices, orig_ranking, emb_knn_indices, label, k, "false")
+	## class-aware continuity
+	ca_cont  = ca_tnc_computation(emb_knn_indices,  emb_ranking, orig_knn_indices, label, k, "missing")
+
+	return {
+		"ca_trustworthiness": ca_trust,
+		"ca_continuity": ca_cont
+	}
+
+def ca_tnc_computation(base_knn_indices, base_ranking, target_knn_indices, label, k, type):
+	"""
+	Core computation of class-aware trustworthiness and continuity
+	"""
+	value = 0.0
+	points_num = base_knn_indices.shape[0]
+
+	for i in range(points_num):
+		# get nearest neighbors that in the target indices but not in the base indices
+		missings = np.setdiff1d(target_knn_indices[i], base_knn_indices[i])
+
+		for missing in missings:
+			if type == "false":
+				if label[i] != label[missing]:
+					value += base_ranking[i, missing] - k
+			elif type == "missing":
+				if label[i] == label[missing]:
+					value += base_ranking[i, missing] - k
+			else:
+				raise ValueError("type should be 'false' or 'missing'")
+
+	return 1 - 2 / (points_num * k * (2 * points_num - 3 * k - 1)) * value
--- test/tnc_mrre_consistency_test.py
+++ test/tnc_mrre_consistency_test.py
@@ -10,8 +10,8 @@ from sklearn.datasets import load_iris, load_digits
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
-digits, _ = load_digits(return_X_y=True)
-iris, _ = load_iris(return_X_y=True)
+digits, digits_label = load_digits(return_X_y=True)
+iris, iris_label = load_iris(return_X_y=True)
 
 pca = PCA(n_components=2)
 
@@ -24,6 +24,7 @@ digits_tsne = tsne.fit_transform(digits)
 iris_tsne = tsne.fit_transform(iris)
 
 from measures.trustworthiness_continuity import trustworthiness_continuity
+from measures.class_aware_trustworthiness_continuity import class_aware_trustworthiness_continuity
 from measures.mean_relative_rank_error import mean_relative_rank_error
 from measures.local_continuity_meta_criteria import local_continuity_meta_criteria
 from provider import MDPMetricProvider
@@ -32,6 +33,8 @@ from provider import MDPMetricProvider
 print(trustworthiness_continuity(digits, digits_pca, 20))
 print(MDPMetricProvider(digits, digits_pca, ["Trustworthiness", "Continuity"], 20).run())
 
+print(class_aware_trustworthiness_continuity(digits, digits_pca, digits_label, 20))
+
 print(mean_relative_rank_error(digits, digits_pca, 20))
 print(MDPMetricProvider(digits, digits_pca, ["MRRE_ZX", "MRRE_XZ"], 20).run())
 
@@ -39,4 +42,5 @@ print(local_continuity_meta_criteria(digits, digits_pca, 20))
 print(local_continuity_meta_criteria(digits, digits_tsne, 20))
 print(local_continuity_meta_criteria(digits, digits_pca, 50))
 print(local_continuity_meta_criteria(digits, digits_tsne, 50))
+
 
