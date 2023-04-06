cffad8529ae4b44c78cc8fabcba82da0fcf5ed3f
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-06 18:17:21 +0900

    added tnc consistency check

--- test/tnc_mrre_consistency_test.py
+++ test/tnc_mrre_consistency_test.py
@@ -24,9 +24,13 @@ iris_pca = pca.fit_transform(iris)
 # iris_tsne = tsne.fit_transform(iris)
 
 from measures.trustworthiness_continuity import trustworthiness_continuity
+from measures.mean_relative_rank_error import mean_relative_rank_error
 from provider import MDPMetricProvider
 
 
 print(trustworthiness_continuity(digits, digits_pca, 20))
 print(MDPMetricProvider(digits, digits_pca, ["Trustworthiness", "Continuity"], 20).run())
+
+print(mean_relative_rank_error(digits, digits_pca, 20))
+print(MDPMetricProvider(digits, digits_pca, ["MRRE_ZX", "MRRE_XZ"], 20).run())
 
