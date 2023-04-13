ffe96dc905da8d50ecda0e21c91a6fb44e5fa464
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-13 20:36:11 +0900

    Added scal.py

--- scalability_eval/scal.py
+++ scalability_eval/scal.py
@@ -1,1 +1,10 @@
 ## TODO // AERI
+
+## Competitors:
+# - ZADU
+# - ZADU without computation scheduling
+# - Previous python implementations/bindings
+
+
+# Check the total runtime of measures
+# - 자주 쓰는거 세개 네개 고르고 그냥 total 시간 (scenario 를 만들어서)
