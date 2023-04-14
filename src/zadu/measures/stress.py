70fe49d54d6f5519cd20f2dd0e0b15e41c31aa99
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-15 05:29:33 +0900

    changed indentation

--- src/zadu/measures/stress.py
+++ src/zadu/measures/stress.py
@@ -2,7 +2,7 @@ from .utils import pairwise_dist as pdist
 import numpy as np
 
 def measure(orig, emb, distance_matrices=None):
-    """
+	"""
 	Compute stress of the embedding
 	INPUT:
 		ndarray: orig: original data
@@ -11,18 +11,18 @@ def measure(orig, emb, distance_matrices=None):
 	OUTPUT:
 		dict: stress
 	"""
-    if distance_matrices is None:
-        orig_distance_matrix = pdist.pairwise_distance_matrix(orig)
-        emb_distance_matrix = pdist.pairwise_distance_matrix(emb)
+	if distance_matrices is None:
+		orig_distance_matrix = pdist.pairwise_distance_matrix(orig)
+		emb_distance_matrix = pdist.pairwise_distance_matrix(emb)
+
+	else:
+		orig_distance_matrix, emb_distance_matrix = distance_matrices
+
+	diff_squared_sum = np.square(orig_distance_matrix - emb_distance_matrix).sum()
+	orig_squared_sum = np.square(orig_distance_matrix).sum()
+
+	stress = np.sqrt(diff_squared_sum / orig_squared_sum)
 	
-    else:
-        orig_distance_matrix, emb_distance_matrix = distance_matrices
-
-    diff_squared_sum = np.square(orig_distance_matrix - emb_distance_matrix).sum()
-    orig_squared_sum = np.square(orig_distance_matrix).sum()
-
-    stress = np.sqrt(diff_squared_sum / orig_squared_sum)
-    
-    return {
-			"stress": stress
+	return {
+		"stress": stress
 	}
