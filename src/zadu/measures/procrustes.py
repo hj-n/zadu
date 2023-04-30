efd84b27612796624e0dbeba65fbc0f9656d2162
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-30 22:49:19 +0900

    revised procrustes and topographic product to be integrated into scheduling pipeline

--- src/zadu/measures/procrustes.py
+++ src/zadu/measures/procrustes.py
@@ -3,37 +3,40 @@ from numpy.linalg import svd
 from .utils import pairwise_dist as pdist
 from .utils import knn
 
-def procrustes(orig, emb, k):
-    """
+def procrustes(orig, emb, k=20, knn_info=None):
+	"""
 	Compute procrustes statistics
 	INPUT:
 		ndarray: orig: original data
 		ndarray: emb: embedded data
-        int: k: number of nearest neighbors to consider
+	      int: k: number of nearest neighbors to consider
 	OUTPUT:
 		normalized procrustes statistics for 
 	"""
-    # k nearest neighbors in original space and embedded space each
-    orig_knn_indices = knn.knn(orig, k)
-    emb_knn_indices  = knn.knn(emb, k)
+	# k nearest neighbors in original space and embedded space each
+	if knn_info is None:
+		orig_knn_indices = knn.knn(orig, k)
+		emb_knn_indices  = knn.knn(emb, k)
+	else:
+		orig_knn_indices, emb_knn_indices = knn_info
 
-    g_list = []
+	g_list = []
 
-    for i in range(orig.shape[0]):
-        origin_neighbors = orig[orig_knn_indices[i]]
-        embedd_neighbors = emb[emb_knn_indices[i]]
+	for i in range(orig.shape[0]):
+		origin_neighbors = orig[orig_knn_indices[i]]
+		embedd_neighbors = emb[emb_knn_indices[i]]
 
-        k = origin_neighbors.shape[0]
-        I = np.eye(k)
-        ones = np.ones((k,k))
-        H = I - (1/k) * ones
+		k = origin_neighbors.shape[0]
+		I = np.eye(k)
+		ones = np.ones((k,k))
+		H = I - (1/k) * ones
 
-        U, _, V_T = svd(origin_neighbors.T @ H @ embedd_neighbors)
-        A = U @ V_T
-        A_T = A.T
+		U, _, V_T = svd(origin_neighbors.T @ H @ embedd_neighbors)
+		A = U @ V_T
+		A_T = A.T
 
-        g = np.linalg.norm(H @ (origin_neighbors - embedd_neighbors @ A_T), ord='fro') ** 2
-        g_normalized = g / np.linalg.norm(H @ origin_neighbors, ord='fro') ** 2
-        g_list.append(g_normalized)
+		g = np.linalg.norm(H @ (origin_neighbors - embedd_neighbors @ A_T), ord='fro') ** 2
+		g_normalized = g / np.linalg.norm(H @ origin_neighbors, ord='fro') ** 2
+		g_list.append(g_normalized)
 
-    return np.mean(g_list)
+	return np.mean(g_list)
--- src/zadu/measures/topographic_product.py
+++ src/zadu/measures/topographic_product.py
@@ -2,42 +2,49 @@ from .utils import pairwise_dist as pdist
 import numpy as np
 from .utils import knn
 
-def measure(orig, emb, k):
-    """
+def measure(orig, emb, k=20, distance_matrices=None, knn_info=None):
+	"""
 	Compute topographic product
 	INPUT:
 		ndarray: orig: original data
 		ndarray: emb: embedded data
-        int: k: number of nearest neighbors to consider
+	      int: k: number of nearest neighbors to consider
 	OUTPUT:
 		topographic product result
 	"""
-    N = len(emb)
-    sum_of_log_p3 = 0
-    
-    orig_distance_matrix = pdist.pairwise_distance_matrix(orig)
-    emb_distance_matrix  = pdist.pairwise_distance_matrix(emb)
+	N = len(emb)
+	sum_of_log_p3 = 0
+	
+	if distance_matrices is None:
+		
+		orig_distance_matrix = pdist.pairwise_distance_matrix(orig)
+		emb_distance_matrix  = pdist.pairwise_distance_matrix(emb)
+	else:
+		orig_distance_matrix, emb_distance_matrix = distance_matrices
 
-    # k nearest neighbors in original space and embedded space each
-    orig_knn_indices = knn.knn(orig, k)
-    emb_knn_indices  = knn.knn(emb, k)
-    
-    for j in range(N):
-        for k in range(N-1):
-            q1_product, q2_product = 1, 1
-            for l in range(k):
-                distance_origin_to_emb_knn      = orig_distance_matrix[j][emb_knn_indices[j][l]]
-                distance_origin_to_origin_knn   = orig_distance_matrix[j][orig_knn_indices[j][l]]
-                q1 = distance_origin_to_emb_knn / distance_origin_to_origin_knn
-                q1_product *= q1
+	# k nearest neighbors in original space and embedded space each
+	if knn_info is None:
+		orig_knn_indices = knn.knn(orig, k)
+		emb_knn_indices  = knn.knn(emb, k)
+	else:
+		orig_knn_indices, emb_knn_indices = knn_info
+	
+	for j in range(N):
+		for k in range(N-1):
+			q1_product, q2_product = 1, 1
+			for l in range(k):
+				distance_origin_to_emb_knn      = orig_distance_matrix[j][emb_knn_indices[j][l]]
+				distance_origin_to_origin_knn   = orig_distance_matrix[j][orig_knn_indices[j][l]]
+				q1 = distance_origin_to_emb_knn / distance_origin_to_origin_knn
+				q1_product *= q1
 
-                distance_emb_to_emb_knn        = emb_distance_matrix[j][emb_knn_indices[j][l]]
-                distance_emb_to_origin_knn     = emb_distance_matrix[j][orig_knn_indices[j][l]]
-                q2 = distance_emb_to_emb_knn   / distance_emb_to_origin_knn
-                q2_product *= q2
+				distance_emb_to_emb_knn        = emb_distance_matrix[j][emb_knn_indices[j][l]]
+				distance_emb_to_origin_knn     = emb_distance_matrix[j][orig_knn_indices[j][l]]
+				q2 = distance_emb_to_emb_knn   / distance_emb_to_origin_knn
+				q2_product *= q2
 
-            p3 = pow(q1_product * q2_product, 1 / (2 * k))
-            sum_of_log_p3 += np.log(p3)
+			p3 = pow(q1_product * q2_product, 1 / (2 * k))
+			sum_of_log_p3 += np.log(p3)
 
-    return sum_of_log_p3 / (N * (N - 1))
+	return sum_of_log_p3 / (N * (N - 1))
             
