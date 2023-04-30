0bf90112ea30027ca78ec371a60d3254a41c7cdb
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-30 23:08:01 +0900

    revised setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-from setuptools import setup, find_packages
- 
-setup(
-    name                = 'metrics4mdp',
-    version             = '0.1.2',
-    description         = 'Quantitative metrics of Multidimensional Projections (MDP) in Python',
-    author              = 'Hyeon Jeon',
-    author_email        = 'hj@hcil.snu.ac.kr',
-    url                 = 'https://github.com/hj-n/metrics4mdp',
-    download_url        = 'https://github.com/hj-n/metrics4mdp',
-    install_requires    =  ['numpy', 'scipy', 'scikit-learn', 'numba'],
-    packages            = find_packages(exclude = []),
-    keywords            = ['MDP', 'Multidimensional Projections', "Dimensionality Reduction", "metric"],
-    python_requires     = '>=3',
-    package_data        = {},
-    zip_safe            = False,
-    classifiers         = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-)
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="zadu",
+	version="0.0.1",
+	author="Hyeon Jeon",
+	author_email="hj@hcil.snu.ac.kr",
+	description="A Python Toolkit for Evaluating the Reliability of Dimensionality Reduction Embeddings",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/hj-n/zadu",
+	classifiers=[
+			"Programming Language :: Python :: 3",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+	],
+	install_requires=[
+		"numpy",
+		"scikit-learn",
+		"scipy",
+		"snc",
+		"matplotlib"
+	],
+	package_dir={"": "src"},
+	packages=setuptools.find_packages(where="src"),
+	python_requires=">=3.9.0",
+)
