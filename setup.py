5d81a5ab3bdd793778365b63570ef82680ab4cee
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-05-01 20:41:10 +0900

    reivsed setup.py for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(setuptools.find_packages(where="src"))
 
 setuptools.setup(
 	name="zadu",
-	version="0.0.8",
+	version="0.1.0",
 	author="Hyeon Jeon",
 	author_email="hj@hcil.snu.ac.kr",
 	description="A Python Toolkit for Evaluating the Reliability of Dimensionality Reduction Embeddings",
